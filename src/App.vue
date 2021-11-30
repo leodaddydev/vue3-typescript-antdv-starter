027a468deb65975115a795584d5470bbc5f896bd
hungvt <hungvt@lattehub.com>
2021-11-30 11:45:49 +0700

    added back to top button

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
   <router-view />
+  <BackToTop />
 </template>
+
+<script lang="ts">
+import BackToTop from "@/components/BackToTop.vue";
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  components: {
+    BackToTop,
+  },
+});
+</script>
